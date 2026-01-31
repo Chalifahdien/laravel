10ea84d5827c7e37b80d227b25bdec1cb76b4e24
Chalifahdien Hamud <hamimu@Chalifahdien-Hamud.local>
2026-02-01 00:15:58 +0800

    perbaiki api machine

--- app/Http/Controllers/Api/MachineController.php
+++ app/Http/Controllers/Api/MachineController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Machine;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Machine;
 
 class MachineController extends Controller
 {
