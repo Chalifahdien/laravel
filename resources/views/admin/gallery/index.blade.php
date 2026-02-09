25a2ccef89d3b1ed20fd38717763569b4c1dbe0e
Chalifahdien Hamud <hamimu@Chalifahdien-Hamud.local>
2026-02-09 19:22:36 +0800

    tes

--- resources/views/admin/gallery/index.blade.php
+++ resources/views/admin/gallery/index.blade.php
@@ -67,22 +67,19 @@
                                     </div>
                                 </div>
 
+                                <div class="card-footer d-flex gap-3">
 
-                                <div class="card-footer">
-                                    <div class="row w-100">
-                                        <div class="col-6">
-                                            <a href="{{ route('transactions.show', $image->session_id) }}"
-                                                class="btn btn-outline-primary w-100">
-                                                DETAIL
-                                            </a>
-                                        </div>
-                                        <div class="col-6">
-                                            <button class="btn btn-outline-danger w-100" data-bs-toggle="modal"
-                                                data-bs-target="#delete{{ $image->id }}">
-                                                Delete
-                                            </button>
-                                        </div>
-                                        {{-- {{ route('transactions.show', $transaction->id) }} --}}
+                                    <div class="col">
+                                        <a href="{{ route('transactions.show', $image->session_id) }}"
+                                            class="btn btn-outline-primary w-100">
+                                            DETAIL
+                                        </a>
+                                    </div>
+                                    <div class="col">
+                                        <button class="btn btn-outline-danger w-100" data-bs-toggle="modal"
+                                            data-bs-target="#delete{{ $image->id }}">
+                                            Delete
+                                        </button>
                                     </div>
                                 </div>
                             </div>
